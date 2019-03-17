c09520b2795bf39713411b5e13e980cd1dee7ba9
Tatiana Uspenskaia <tanyauspenskaya@gmail.com>
2019-03-17 17:41:08 -0400

    polaris button

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import { AppProvider, Page, Card, Button } from '@shopify/polaris';
+
 
 class App extends Component {
   render() {
-    return <div>app</div>;
+    return(
+      <AppProvider>
+        <Page title="Example app">
+          <Card sectioned>
+            <Button onClick={() => alert('Button clicked!')}>Example button</Button>
+          </Card>
+        </Page>
+      </AppProvider>
+    );
   }
 }
 
